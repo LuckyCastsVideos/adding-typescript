1719c7bd29dc4559ffbcd238770327abf43baec0
Stephen Dolan <dolan.stephen1@gmail.com>
2020-09-03 19:36:05 -0400

    Final

--- webpack.mix.js
+++ webpack.mix.js
@@ -44,7 +44,7 @@ mix
   //
   // More info and options (like React support) here:
   // https://github.com/JeffreyWay/laravel-mix/blob/master/docs/mixjs.md
-  .js("src/js/app.js", "public/js")
+  .ts("src/js/app.ts", "public/js/app.js")
   // SASS entry file. Uses autoprefixer automatically.
   .sass("src/css/app.scss", "public/css")
   // Customize postCSS:
